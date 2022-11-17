6e9f1b0f058489a7c561d0face787547efa95429
hyunsu.An <ahs39490215@gmail.com>
2022-11-17 10:04:09 +0900

    수정

--- 안현수/문자열안에문자열.js
+++ 안현수/문자열안에문자열.js
@@ -1,7 +1,3 @@
 function solution(str1, str2) {
-<<<<<<<<< Temporary merge branch 1
   return !str1.match(str2) ? 1 : 2;
-=========
-  return !str1.match(str2) ? 1 : 2; //변경
->>>>>>>>> Temporary merge branch 2
 }
