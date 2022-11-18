c19660692100ab83c06ed37cddd90f1032cff592
sandwe <preengus@gmail.com>
2022-11-19 00:59:42 +0900

    Solve: 369 게임, 모스부호(1) 해결

--- 서현주/369게임.js
+++ 서현주/369게임.js
@@ -1,1 +1,4 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120891
+function solution(order) {
+  return [...order.toString()].reduce((result, num) => (num == 3 || num == 6 || num == 9 ? ++result : result), 0);
+}
