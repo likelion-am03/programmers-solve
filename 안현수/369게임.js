de524f4cd8132ba49173efad147726eb36c46c6e
hyunsu.An <ahs39490215@gmail.com>
2022-11-20 10:29:25 +0900

    Solve: 모스부호(1),369게임

--- 안현수/369게임.js
+++ 안현수/369게임.js
@@ -1,12 +1,3 @@
 function solution(order) {
-  let answer = 0;
-  order
-    .toString()
-    .split("")
-    .forEach((v) => {
-      if (v == "3" || v == "6" || v == "9") {
-        answer++;
-      }
-    });
-  return answer;
+  return ((order + "").match(/['3'|'6'|'9']/g) || []).length;
 }
