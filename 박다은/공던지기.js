1917a28a269b4ae557565f70948be182ba71f687
ekdms5566 <ekdms5566@naver.com>
2022-12-07 16:21:27 +0900

    Solve : 공던지기, 문자열밀기 해결

---
+++ 박다은/공던지기.js
@@ -0,0 +1,5 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120843
+
+function solution(numbers, k) {
+  return numbers[((k - 1) * 2) % numbers.length];
+}
---
+++ 박다은/문자열밀기.js
@@ -0,0 +1,3 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120921
+
+let solution = (a, b) => (b + b).indexOf(a);
